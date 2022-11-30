9c9281ff48bd8eaad5bbc0e82abcdc79e93f482b
Siwakorn-me <Siwakorn.me2024@kmids.ac.th>
2022-11-30 08:29:09 +0700

    Done

---
+++ tuples-assignment.py
@@ -0,0 +1,25 @@
+"""Write a function called skip_tuples. It has a tuple as input. It returns a new tuple as output, such as every second element of the input tuple is skipped, starting with the first one.
+
+Example:
+
+The input tuple is('I', 'am', 'a', 'test', 'tuple')
+
+This will return the tuple ('I', 'a', 'tuple').
+
+Don’t forget to write a docstring for the above function.
+
+Put your testcases in main()"""
+
+tuple = ('I', 'am', 'a', 'test', 'tuple')
+
+
+def skip_tuples(a):
+    newTuple = ()
+    for num in range(len(a)):
+        print(num)
+        if num % 2 == 0:
+            newTuple = newTuple + (a[num],)
+    return newTuple
+
+
+print(skip_tuples(tuple))
